3490445721b766bee31006ee91c48d5ea75515eb
Michael <Michael_MaldonadoJr1@Baylor.edu>
2023-02-08 13:38:51 -0600

    Finished

--- 3_simple_exercises.py
+++ 3_simple_exercises.py
@@ -13,15 +13,17 @@ sampleDict = {
    }
 }
 
-
+score = sampleDict["class"]["student"]["marks"]["history"]
+print(score)
 
 
 # 2) Add 2 inches to the son's height.
 
 dict={"son's name": "Lucas", "son's eyes": "green", "son's height": 32, "son's weight": 25}
 
+dict["son's height"] += 2
 
-
+print(dict)
 
 
 # 3) Given a Python dictionary, Change Brad’s salary to 8500
@@ -32,7 +34,9 @@ sampleDict = {
      'emp3': {'name': 'Brad', 'salary': 6500}
 }
 
+sampleDict["emp3"]['salary'] = 8500
 
+print(sampleDict)
 
 
 
@@ -43,3 +47,6 @@ sampleDict = {
 dict={"name": "Plato", "country": "Ancient Greece", "born": -427, "teacher": "Socrates", "student": "Aristotle"}
 
 
+dict["work"] = ["Apology", "Phaedo", "Republic", "Symposium"]
+
+print(dict)
